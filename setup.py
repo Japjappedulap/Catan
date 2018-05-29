c05f604823bc9ad7640612c6eb1ca2ca8630e8f0
pogra <pvie2207@scs.ubbcluj.ro>
2018-05-29 14:47:22 +0300

    Added setup.py

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+    name='Catan',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='Vlad Potra',
+    author_email='potravlad@gmail.com',
+    description='',
+    install_requires=['numpy']
+)
