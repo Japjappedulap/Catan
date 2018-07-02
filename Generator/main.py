40edb38eceab8c45b6889de460f4d529c38215c4
pogra <pvie2207@scs.ubbcluj.ro>
2018-07-02 22:39:52 +0300

    Mit license added

--- Generator/main.py
+++ Generator/main.py
@@ -4,6 +4,31 @@ from Generator.ClassicMap import ClassicMap
 from Generator.ExtendedMap import ExtendedMap
 
 
+def dummy():
+    import json
+    return json.dumps({"tile": [
+        {"index": 0, "dice": 0, "resource_type": "DESE"},
+        {"index": 1, "dice": 5, "resource_type": "OREE"},
+        {"index": 2, "dice": 2, "resource_type": "WOOL"},
+        {"index": 3, "dice": 6, "resource_type": "CLAY"},
+        {"index": 4, "dice": 11, "resource_type": "GRAI"},
+        {"index": 5, "dice": 10, "resource_type": "LUMB"},
+        {"index": 6, "dice": 6, "resource_type": "OREE"},
+        {"index": 7, "dice": 3, "resource_type": "LUMB"},
+        {"index": 8, "dice": 4, "resource_type": "OREE"},
+        {"index": 9, "dice": 8, "resource_type": "WOOL"},
+        {"index": 10, "dice": 12, "resource_type": "GRAI"},
+        {"index": 11, "dice": 9, "resource_type": "CLAY"},
+        {"index": 12, "dice": 9, "resource_type": "GRAI"},
+        {"index": 13, "dice": 3, "resource_type": "LUMB"},
+        {"index": 14, "dice": 4, "resource_type": "CLAY"},
+        {"index": 15, "dice": 8, "resource_type": "LUMB"},
+        {"index": 16, "dice": 10, "resource_type": "WOOL"},
+        {"index": 17, "dice": 5, "resource_type": "GRAI"},
+        {"index": 18, "dice": 11, "resource_type": "WOOL"}],
+        "type": "classic"})
+
+
 def classic():
     no_player = 5  # desert on corner
     # no_player = 6  # desert in center
